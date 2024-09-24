9cefb574a4b23aa153fede029dd8d360f6baec3e
tsukasa <chappieoct4@gmail.com>
2024-09-25 08:43:05 +0900

    追記：use SoftDeletesをModelに記述

--- bistrobucks/app/Models/DailyCost.php
+++ bistrobucks/app/Models/DailyCost.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DailyCost extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes; // ファクトリーを使う、ソフトデリートを使う
 
     // リレーションを定義
     public function shop() // shopsテーブルとのリレーション
