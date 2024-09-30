bb0fb4c0b7d6cc7b71fa70a8eff9a4b4757b86a3
tsukasa <chappieoct4@gmail.com>
2024-10-01 00:46:12 +0900

    修正：table設計変更に伴い

--- bistrobucks/database/migrations/2024_09_23_194557_create_products_table.php
+++ bistrobucks/database/migrations/2024_09_23_194557_create_products_table.php
@@ -17,10 +17,18 @@ return new class extends Migration
             $table->string('name');
             $table->string('category')->nullable();  // 商品カテゴリ
             $table->decimal('unit_price', 8, 2);  // 税込価格
+            $table->date('start_date')->nullable();  // 販売開始日
+            $table->date('end_date')->nullable();  // 販売終了日
+            $table->string('image_path')->nullable();  // 商品画像
+            $table->string('description')->nullable();  // 商品説明
+            $table->integer('version')->default(1);  // バージョン(バージョン管理)
+            $table->softDeletes(); // 論理削除
             $table->timestamps();
 
-            // 外部キー制約
+            // 外部キー制約・・・shop_idはshopsテーブルのidと紐づく
             $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
+            // ユニーク制約(商品名とバージョン)・・・同じ商品名でもバージョンが違えば登録できる
+            $table->unique(['name', 'version']);
         });
     }
 
