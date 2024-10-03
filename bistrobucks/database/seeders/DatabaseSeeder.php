de16e4a1e73942e0dba9ac039584747e470da51a
tsukasa <chappieoct4@gmail.com>
2024-10-04 08:29:24 +0900

    追記：User,Shopのfactory実行コード

--- bistrobucks/database/seeders/DatabaseSeeder.php
+++ bistrobucks/database/seeders/DatabaseSeeder.php
@@ -5,11 +5,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\DailyCost;
-use App\Models\Product;
-use App\Models\Sale;
-use App\Models\SalesDetail;
-use App\Models\Setting;
 use App\Models\Shop;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -21,13 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //各モデル毎にファクトリーを呼び出す。リレーションはファクトリーで設定しているので、ここで呼び出すだけでリレーションが設定される。
-        User::factory(10)->count(10)->create();
-        Sale::factory(10)->count(10)->create();
-        SalesDetail::factory(10)->count(10)->create();
-        Product::factory(10)->count(10)->create();
-        Shop::factory(10)->count(10)->create();
-        DailyCost::factory(10)->count(10)->create();
-        Setting::factory(10)->count(10)->create();
+        // // User::factory(10)->create();
+        // Shop::factory(10)->create();
     }
 }
