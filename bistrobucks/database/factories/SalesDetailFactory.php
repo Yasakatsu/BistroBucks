3cfbf044cd5c73549d541f2ba08c31508c98b4da
tsukasa <chappieoct4@gmail.com>
2024-10-05 14:24:20 +0900

    SalesDetailFactoryを更新し、セール詳細フィールドを含める

--- bistrobucks/database/factories/SalesDetailFactory.php
+++ bistrobucks/database/factories/SalesDetailFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,12 @@ class SalesDetailFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'sale_id' => \App\Models\Sale::factory(),
+            'product_name_id' => Product::factory(),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'item_price' => $this->faker->randomFloat(2, 0, 1000),
+            'item_price_excluding_tax' => $this->faker->randomFloat(2, 0, 1000),
+            'sale_amount' => $this->faker->randomFloat(2, 0, 1000),
         ];
     }
 }
