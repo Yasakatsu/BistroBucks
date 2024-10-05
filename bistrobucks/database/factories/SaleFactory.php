499c590b5ac194f8d090267e08a7664019fb0e69
tsukasa <chappieoct4@gmail.com>
2024-10-05 14:23:12 +0900

    SaleFactoryを更新してshop_id、tax_rate_id、その他のフィールドを含める

--- bistrobucks/database/factories/SaleFactory.php
+++ bistrobucks/database/factories/SaleFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Shop;
+use App\Models\TaxRate;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,18 @@ class SaleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'shop_id' => Shop::factory(),
+            'tax_rate_id' => TaxRate::factory(),
+            'total_sale_amount' => $this->faker->randomFloat(2, 0, 1000),
+            'discount_amount' => $this->faker->randomFloat(2, 0, 1000),
+            'tax_amount' => $this->faker->randomFloat(2, 0, 1000),
+            'customer_amount' => $this->faker->numberBetween(1, 100),
+            'new_customer_amount' => $this->faker->numberBetween(1, 100),
+            'repeat_customer_amount' => $this->faker->numberBetween(1, 100),
+            'referral_customer_amount' => $this->faker->numberBetween(1, 100),
+            'payment_detail' => $this->faker->randomElement(['現金', 'クレジットカード', 'デビットカード', '電子マネー', 'QRコード', 'その他']),
+            'consumption_type' => $this->faker->randomElement(['店内飲食', 'テイクアウト']),
+            'date' => $this->faker->date(),
         ];
     }
 }
--- bistrobucks/database/migrations/2024_09_30_225019_create_sales_table.php
+++ bistrobucks/database/migrations/2024_09_30_225019_create_sales_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('shop_id');  // 外部キー
+            $table->unsignedBigInteger('tax_rate_id');  // 外部キー
             $table->decimal('total_sale_amount', 8, 2);  // 売上金額(8桁の整数部分と2桁の小数部分. 例: 12345.67/ 1000円の場合は1000.00/ 100円の場合は100.00/ 10円の場合は10.00)
             $table->decimal('discount_amount', 8, 2)->nullable();  // 割引金額(8桁の整数部分と2桁の小数部分/ 例: 12345.67/ 1000円の場合は1000.00/ 100円の場合は100.00/ 10円の場合は10.00)
-            $table->decimal('tax_rate', 5, 2);  // 税率(5桁の整数部分と2桁の小数部分/ 例: 12.34/ 10%の場合は10.00/ 8%の場合は8.00)
             $table->decimal('tax_amount', 8, 2);  // 税金額(8桁の整数部分と2桁の小数部分/ 例: 12345.67/ 1000円の場合は1000.00/ 100円の場合は100.00/ 10円の場合は10.00)
             $table->integer('customer_amount'); // 来客人数(必須/ 例: 1/ 2/ 3)
             $table->integer('new_customer_amount');  // 新規顧客の人数(必須)
@@ -28,6 +28,8 @@ return new class extends Migration
             $table->timestamps();
             // shopsテーブルのidカラムに存在しない値を挿入しようとするとエラーが発生する
             $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
+            // tax_ratesテーブルのidカラムに存在しない値を挿入しようとするとエラーが発生する
+            $table->foreign('tax_rate_id')->references('id')->on('tax_rates')->onDelete('cascade');
         });
     }
 
