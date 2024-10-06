bcbd25a12b81f77ff76f45a4aea7625f036848bb
tsukasa <chappieoct4@gmail.com>
2024-10-06 23:18:22 +0900

    sail_details との関連付けが欠けていたのを修正。

--- bistrobucks/routes/api.php
+++ bistrobucks/routes/api.php
@@ -1,33 +1,8 @@
 <?php
 
-use App\Http\Controllers\DailyCostController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SaleController;
-use App\Http\Controllers\SalesDetailController;
-use App\Http\Controllers\SettingController;
-use App\Http\Controllers\ShopController;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::middleware('auth')->group(function () {
-    // UserControllerのresourceを定義
-    Route::resource('users', UserController::class);
-    // ProductControllerのresourceを定義
-    Route::resource('products', ProductController::class);
-    // SaleControllerのresourceを定義
-    Route::resource('sales', SaleController::class);
-    // SalesDetailControllerのresourceを定義
-    Route::resource('sales-details', SalesDetailController::class);
-    // DailyCostControllerのresourceを定義
-    Route::resource('daily-costs', DailyCostController::class);
-    // SettingControllerのresourceを定義
-    Route::resource('settings', SettingController::class);
-    // ShopControllerのresourceを定義
-    Route::resource('shops', ShopController::class);
-});
