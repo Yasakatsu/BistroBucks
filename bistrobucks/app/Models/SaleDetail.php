0c432a1d06de3bada99b7c4e9514fe55e5cbd33e
tsukasa <chappieoct4@gmail.com>
2024-10-09 08:16:06 +0900

    初期値にproductsテーブルitem_priceが入るように実装

--- bistrobucks/app/Models/SaleDetail.php
+++ bistrobucks/app/Models/SaleDetail.php
@@ -8,23 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class SaleDetail extends Model
 {
     use HasFactory;
-
     protected $fillable = [
         'sale_id',
-        'item_name',
+        'product_id',
         'quantity',
         'item_price',
         'item_price_excluding_tax',
         'sale_amount',
     ];
 
+    //********************** */
+    // リレーション定義
+    //********************** */
     public function sale()
     {
         return $this->belongsTo(Sale::class);
     }
-
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    // SaleDetailモデルが初期化されるときに自動的に呼び出されるメソッド
+    protected static function boot()
+    {
+        // bootメソッドを使用する時のおまじない
+        parent::boot();
+
+        // SaleDetailモデルが作成されるときに自動的に呼び出される
+        static::creating(function ($saleDetailInstance) {
+            if (is_null($saleDetailInstance->item_price)) {
+                $product = Product::find($saleDetailInstance->product_id);
+                $saleDetailInstance->item_price = $product->unit_price;
+            }
+        });
     }
 }
