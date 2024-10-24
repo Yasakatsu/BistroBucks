7620ccac1189672c4b40ed355a392ad6f9a9b903
tsukasa <chappieoct4@gmail.com>
2024-10-25 08:04:13 +0900

    リファクタリング

--- bistrobucks/resources/js/Pages/Dashboard.jsx
+++ bistrobucks/resources/js/Pages/Dashboard.jsx
@@ -264,9 +264,6 @@ export default function Dashboard(props) {
                         </Box>
                     </>
                 )}
-                <Card variant="elevated">
-                    
-                </Card>
             </VStack>
         </>
     );
